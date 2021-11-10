21bd2a51705f7db2d2372d60825a30a4062efcc4
David Munoz <dmp.david@gmail.com>
2021-11-10 14:26:39 +0100

    Ejercicios terminados

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,12 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'David';
+let hp: number | string = 96;
+let estaVivo: boolean = true;
+
+
+hp = 'FULL';
+
+console.log( nombre, hp );
